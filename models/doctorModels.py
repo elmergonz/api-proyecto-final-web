31df42dd441cda875c5ca7ed8ce2511f63747edb
elmergonz <elmer.go@outlook.com>
2020-11-22 13:08:43 -0400

    ahora los modelos tienen propiedades de agregar como dict

---
+++ models/doctorModels.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class Base(BaseModel):
+
+    def __setitem__(self, key, value):
+        self.__dict__[key] = value
+    
+    def __getitem__(self, key):
+        return self.__dict__[key]
+
+class Doctor(Base):
+    name: str = ''
+    email: str = ''
+    password: str = ''
+    
+    # token:str = ''
+
+class DoctorLog(Base):
+    email: str = ''
+    password: str = ''
+
+class DoctorData(Base):
+    name: Optional[str] = None
+    email: Optional[str] = None
+
+p = Doctor()
+
+p['token'] = '123456'
+
+for i in p:
+    print(i)
