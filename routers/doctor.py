557467ec8ca6af53ce5b8664710de19b4e21dd23
elmergonz <elmer.go@outlook.com>
2020-11-22 13:09:14 -0400

    registrar ya esta completado, per en pruebas

---
+++ routers/doctor.py
@@ -0,0 +1,37 @@
+from models.doctorModels import *
+from models.serverResult import ServerResult
+from db import dbdata
+
+from fastapi import APIRouter
+import secrets
+import re
+
+router = APIRouter()
+
+@router.post('/api/doctor/register')
+async def register(doctor:Doctor):
+
+    doc = await dbdata.get_doctor_by_email(doctor.email)
+
+    if not doc:
+        if not check_email(doctor.email):
+            return ServerResult(ok=False, message="Agrege una direccion de correo valida")
+        
+        doctor['token'] = secrets.token_urlsafe(20)
+
+        newDoctor = await dbdata.add_doctor(doctor)
+
+        # Si se creo correctamente...
+        if newDoctor:
+            return ServerResult(response={
+                'token': doctor['token']
+            }, message="Doctor creado correctamente")
+    else:
+        return ServerResult(ok=False, message="Este doctor ya existe")
+
+
+
+def check_email(email:str):
+    if re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
+        return True
+    return False
